8cbb2b0edf3d16ddf9638ed21ac07b764babd885
mgamez <mgamez@grupo-ari.net>
2018-01-24 00:20:59 -0400

    Start project

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np 
+from numpy import sqrt
+from scipy.optimize import fsolve
+
+b = 0
+c = -4
+a = 1
+
+data = (a, b, c)
+
+def resolvente(x, *data):
+	a, b, c = data
+	return - x + (-b - sqrt( b**2 - 4 * a * c) / (2 * a))
+
+x0 = -0.1
+
+#a = resolvente(a0, *data)
+
+x = fsolve(resolvente, x0, args=data)
+
+for i in range(0, 11, 2):
+	print(i)
+
+
