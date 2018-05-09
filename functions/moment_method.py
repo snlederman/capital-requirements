168845282cb24664a3baef4abadfaad779f66b04
Samuel Lederman <samy91@gmail.com>
2018-05-09 12:58:44 -0400

    montecarlo

--- functions/moment_method.py
+++ functions/moment_method.py
@@ -1,53 +1,54 @@
-import csv
 import pandas as pd
 import timeit
 from scipy.stats import norm, multivariate_normal
 import numpy as np
 from scipy.optimize import fsolve
 
-start = timeit.default_timer()
-
-
-with open('/users/samuelledermansardi/PycharmProjects/Tesis/capital-requirements/data/default.csv', newline='') as f:
-    reader = csv.reader(f)
-    data = {'year':[], 'Dt':[], 'Nt':[]}
-    for row in reader:
-        data['year'].append(int(row[0]))
-        data['Dt'].append(int(row[1]))
-        data['Nt'].append(int(row[2]))
-
-df_data = pd.DataFrame(data=data).sort_values('year')
-
-P1 = sum(df_data.Dt / df_data.Nt) / len(df_data.year)
-P2 = sum((df_data.Dt * (1 - df_data.Dt)) / (df_data.Nt * (1 - df_data.Nt))) / len(df_data.year)
-
-d = norm.ppf(P1)
-
-x = np.array([d, d])
-rho = 0.30708572
-cov = np.array([[1, rho], [rho, 1]])
-mean = np.array([0,0])
-
-#print(multivariate_normal.cdf(x, mean, cov))
-print(P2)
-
 def fun(rho, *args):
-    d, mean = args
+    d, P2 = args
     x = np.array([d, d])
-    mean = np.array([0,0])
+    mean = np.array([0, 0])
     cov = np.array([[1, rho], [rho, 1]])
     return P2 - multivariate_normal.cdf(x, mean, cov)
 
-x0 = 0
-rho = fsolve(fun, x0, args=(d,mean))
-x = np.array([d, d])
-cov = np.array([[1, rho], [rho, 1]])
-mean = np.array([0,0])
+def correlation(data, DEBUG=False):
+    start = timeit.default_timer()
 
-print(multivariate_normal.cdf(x, mean, cov))
+    df_data = pd.DataFrame(data=data).sort_values('year') # Creamos un DataFrame y lo ordenamos por agno.
 
+    '''
+        Calculamos los momentos para utilizarlo en la estimacion de la correlacion.
+            - P1: momento 1
+            - P2: momento 2
+    '''
+    P1 = sum(df_data.Dt / df_data.Nt) / len(df_data.year)
+    P2 = sum((df_data.Dt * (1 - df_data.Dt)) / (df_data.Nt * (1 - df_data.Nt))) / len(df_data.year)
 
-stop = timeit.default_timer()
-execution_time = stop - start
+    '''
+        d: La inversa de una funcion de distribucion acumulada de una normal estandar.
+    '''
+    d = norm.ppf(P1)
 
-print("Program Executed in {} seconds".format(execution_time))
+    rho = fsolve(fun, x0=0, args=(d, P2))
+
+    countries = {'Argentina':[], 'Bolivia':[], 'Brasil':[], 'Chile':[], 'Colombia':[],
+    'Costa Rica':[], 'Ecuador':[], 'El Salvador':[], 'Guatemala':[],
+    'Hoduras':[], 'Mexico':[], 'Panama':[], 'Paraguay':[], 'Peru':[],
+    'Republica Dominicana':[], 'Uruguay':[],
+    'Venezuela':[]}
+    for i in range(10 ** 6):
+        for country in countries.keys():
+            countries[country].append(np.sqrt(rho) * np.random.normal(0, 1) + np.sqrt(1 - rho) * np.random.normal(0, 1))
+
+    df_sol = pd.DataFrame(data=countries)
+    print(df_sol)
+
+    stop = timeit.default_timer()
+    execution_time = stop - start
+
+    if DEBUG:
+        print(multivariate_normal.cdf(x=np.array([d, d]), mean = np.array([0,0]), cov=np.array([[1, rho], [rho, 1]])))
+        print(P2)
+        print("moment_method Program Executed in {} seconds".format(execution_time))
+
+    return rho
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
-import functions.mat_trans
-import functions.moment_method
-from numpy import matrix, zeros
+#from functions.mat_trans import mt
+from functions.moment_method import correlation
 import csv
-###################################### DATA #######################################################################
 
+############################################ DATA ######################################################################
+'''
+    - Se abre el documento, guardado en la carpeta de data.
+    - Se crea una variable reader que contiene toda la informacion del documento.
+    - Se crea un diccionario con los valores de cada columna.
+    - Se ejecuta un for para ir tomando los valores contenido en reader.
+'''
+with open('data/data.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = {'id':[], 'rating':[], 'date':[]}
+    for row in reader:
+        data['id'].append(int(row[0]))
+        data['date'].append(row[1])
+        data['rating'].append(row[2])
+data_mt = data
 
+with open('data/default.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = {'year':[], 'Dt':[], 'Nt':[]}
+    for row in reader:
+        data['year'].append(int(row[0]))
+        data['Dt'].append(int(row[1]))
+        data['Nt'].append(int(row[2]))
+data_mm = data
 
+'''
+ Loss Given Default.
+ Perdidas Dado el Incumplimiento.
+'''
+LGD = 45
+'''
+ Expoture At Default
+ Exposicion ante el incumplimiento
+'''
+EAD = 100
 
+############################################ MODEL #####################################################################
+#Mt = mt(data_mt)
+rho = correlation(data_mm, DEBUG=True)
 
-
-# Loss Given Default.
-# Perdidas Dado el Incumplimiento.
-#LGD = 45
-
-# Expoture At Default
-# Exposicion ante el incumplimiento
-#EAD = 100
-
-#A = zeros((17,17))
-#print(A[1][1])
 
 
 
