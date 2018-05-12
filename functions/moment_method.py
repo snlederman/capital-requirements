d87e687c3c66cc6edfce1a9e7ccf754a372e62c9
Kleiver Fonseca <kfonseca@grupo-ari.net>
2018-05-12 00:53:44 -0400

    Usar multiprocesos.

--- functions/moment_method.py
+++ functions/moment_method.py
@@ -1,9 +1,10 @@
-import pandas as pd
 import timeit
-from scipy.stats import norm, multivariate_normal
-import numpy as np
-from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.optimize import fsolve
+from scipy.stats import norm, multivariate_normal
+
 
 def fun(rho, *args):
     d, P2 = args
@@ -12,31 +13,32 @@ def fun(rho, *args):
     cov = np.array([[1, rho], [rho, 1]])
     return P2 - multivariate_normal.cdf(x, mean, cov)
 
+
 def correlation(data, sol, LGD, EAD, DEBUG=False):
     start = timeit.default_timer()
 
-    df_data = pd.DataFrame(data=data).sort_values('year') # Creamos un DataFrame y lo ordenamos por agno.
-    df_sol = pd.DataFrame(data=sol).sort_values('country') # TODO: comentar
-    '''
+    df_data = pd.DataFrame(data=data).sort_values('year')  # Creamos un DataFrame y lo ordenamos por agno.
+    df_sol = pd.DataFrame(data=sol).sort_values('country')  # TODO: comentar
+    """
         Calculamos los momentos para utilizarlo en la estimacion de la correlacion.
             - P1: momento 1
             - P2: momento 2
-    '''
+    """
     P1 = sum(df_data.Dt / df_data.Nt) / len(df_data.year)
     P2 = sum((df_data.Dt * (1 - df_data.Dt)) / (df_data.Nt * (1 - df_data.Nt))) / len(df_data.year)
 
-    '''
+    """
         d: La inversa de una funcion de distribucion acumulada de una normal estandar.
-    '''
+    """
     d = norm.ppf(P1)
 
     rho = fsolve(fun, x0=np.array(0), args=(d, P2))
 
-    countries = {'Argentina':[], 'Bolivia':[], 'Brasil':[], 'Chile':[], 'Colombia':[],
-    'Costa Rica':[], 'Ecuador':[], 'El Salvador':[], 'Guatemala':[],
-    'Hoduras':[], 'Mexico':[], 'Panama':[], 'Paraguay':[], 'Peru':[],
-    'Republica Dominicana':[], 'Uruguay':[],
-    'Venezuela':[]}
+    countries = {'Argentina': [], 'Bolivia': [], 'Brasil': [], 'Chile': [], 'Colombia': [],
+                 'Costa Rica': [], 'Ecuador': [], 'El Salvador': [], 'Guatemala': [],
+                 'Hoduras': [], 'Mexico': [], 'Panama': [], 'Paraguay': [], 'Peru': [],
+                 'Republica Dominicana': [], 'Uruguay': [],
+                 'Venezuela': []}
     loss = []
     for i in range(10):
         z = np.random.normal(0, 1)
@@ -54,10 +56,10 @@ def correlation(data, sol, LGD, EAD, DEBUG=False):
     execution_time = stop - start
 
     if DEBUG:
-        '''
+        """
         print(multivariate_normal.cdf(x=np.array([d, d]), mean = np.array([0,0]), cov=np.array([[1, rho], [rho, 1]])))
         print(P2)
-        '''
+        """
         print("moment_method Program Executed in {} seconds".format(execution_time))
 
     return rho
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from functions.mat_trans import mt
-from functions.moment_method import correlation
 import csv
 
+from functions.mat_trans import mt
+from functions.moment_method import correlation
+
 ############################################ DATA ######################################################################
-'''
+"""
     - Se abre el documento, guardado en la carpeta de data.
     - Se crea una variable reader que contiene toda la informacion del documento.
     - Se crea un diccionario con los valores de cada columna.
     - Se ejecuta un for para ir tomando los valores contenido en reader.
-'''
+"""
 with open('data/data.csv', newline='') as f:
     reader = csv.reader(f)
-    data = {'id':[], 'rating':[], 'date':[]}
+    data = {'id': [], 'rating': [], 'date': []}
     for row in reader:
         data['id'].append(int(row[0]))
         data['date'].append(row[1])
@@ -20,30 +21,24 @@ data_mt = data
 
 with open('data/default.csv', newline='') as f:
     reader = csv.reader(f)
-    data = {'year':[], 'Dt':[], 'Nt':[]}
+    data = {'year': [], 'Dt': [], 'Nt': []}
     for row in reader:
         data['year'].append(int(row[0]))
         data['Dt'].append(int(row[1]))
         data['Nt'].append(int(row[2]))
 data_mm = data
 
-'''
+"""
  Loss Given Default.
  Perdidas Dado el Incumplimiento.
-'''
+"""
 LGD = .45
-'''
+"""
  Expoture At Default
  Exposicion ante el incumplimiento
-'''
+"""
 EAD = 100
 
 ############################################ MODEL #####################################################################
 sol = mt(data_mt)
 rho = correlation(data_mm, sol, LGD, EAD)
-
-
-
-
-
-
